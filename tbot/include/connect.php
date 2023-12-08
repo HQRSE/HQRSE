5f6746f14d79f893e9caf1cfe45174619468d2c9
HQRSE <60251201+HQRSE@users.noreply.github.com>
2023-12-08 15:46:13 +0300

    Create connect.php

---
+++ tbot/include/connect.php
@@ -0,0 +1,10 @@
+<?
+/* *** */
+$servername = "localhost";
+$username = "u1182208_hqrse";
+$password = "]xSK7v1!p5";
+$dbname = "u1182208_hqrse";
+$link = mysqli_connect($servername, $username, $password, $dbname);
+mysqli_set_charset($link, "utf8");
+/* *** */
+?>
